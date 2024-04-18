02ab2e63995e763fd22c3f76901332291ce9584b
MaicolSmith <maicol.smith.bueno@gmail.com>
2024-04-17 20:44:01 -0500

    respuesta 3

---
+++ respuesta3.js
@@ -0,0 +1,8 @@
+//Dado el siguiente objeto: let persona = { nombre: 'Juan', edad: 30,
+//profesion: 'Ingeniero'} , accede al valor de la propiedad "nombre" e
+//imprímelo en la consola utilizando la notación de punto y la notación de corchetes.
+let persona = { nombre: 'Juan', edad: 30, profesion: 'Ingeniero' };
+
+console.log(persona.nombre);
+
+console.log(persona['nombre']);
